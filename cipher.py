cf2948e733839fbfa1d82c826941c7f704eb3d94
EvelynGao233 <ygao@smith.edu>
2022-02-20 04:12:39 -0500

    Added my project

--- cipher.py
+++ cipher.py
@@ -1,7 +1,8 @@
 import random
 import sys
 import getopt
-#generate one-time-pad
+
+#generate one-time-pad in given size 
 def generatePad(size):
 #open file and use random to generate random alphabet 
   with open ("One_time_pad.txt","w") as file:
@@ -13,9 +14,9 @@ def generatePad(size):
   
   return pad
 
-# decipher
+# decipher given file with generated one-time-pad
 def decipher(filename,pad):
-
+  # open file, one-time-pad, read and save string 
   message = open(filename, "r").read()
   pad = open(pad,"r").read()
   decipher = ""
@@ -23,18 +24,28 @@ def decipher(filename,pad):
 #if it is alphabet, use pad to decipher, otherwise add the original index element
   for i in range (len(message)):
     if message[i].isalpha()== True:
-      asc = ord(message[i])      
-      asc_pad = ord(pad[count])-65      
+      #ascii number of the alphabet
+      asc = ord(message[i])
+      #ascii number of the pad
+      asc_pad = ord(pad[count])-65
+      #decipher, substract the ascii number of the pad
       de_asc = asc - asc_pad
+      #if original number is uppercase and after substracting pad ascii number it becomes lower than 65,
+      #add 26 to get correct decipher alphabet
       if 64 < asc < 91:
         if de_asc < 65:
           de_asc = de_asc + 26
+      #if original number is lowercase and after substracting pad ascii number it becomes lower than 97,
+      #add 26 to get correct decipher alphabet
       else: 
         if de_asc < 97:
-          de_asc = de_asc + 26        
+          de_asc = de_asc + 26
+      # use chr to get the alphabet and add to decipher
       decipher += chr(de_asc)
+      #move to next alphabet in one-time-pad
       count += 1
     else:
+      #not alphabet, directly add to decipher
       decipher += message[i]
 
   return decipher
@@ -48,23 +59,34 @@ def encipher(filename,pad):
 #if it is alphabet, use pad to encipher, otherwise add the original index element
   for i in range (len(message)):
     if message[i].isalpha()== True:
-      asc = ord(message[i])      
+      #ascii number of the alphabet
+      asc = ord(message[i])
+      #ascii number of the pad
       asc_pad = ord(pad[count])-65
+      #encipher, add the ascii number of the pad
       en_asc = asc + asc_pad
+      #if original number is uppercase and after adding pad ascii number it becomes larger than 90,
+      #substract 26 to get correct encipher alphabet
       if 64 < asc < 91:
         if en_asc > 90:
           en_asc = en_asc - 26
+      #if original number is lowercase and after adding pad ascii number it becomes larger than 122,
+      #substract 26 to get correct encipher alphabet
       else: 
         if en_asc > 122:
           en_asc = en_asc - 26
+      # use chr to get the alphabet and add to encipher
       encipher += chr(en_asc)
+      #move to next alphabet in one-time-pad
       count += 1
     else:
+      #not alphabet, directly add to encipher
       encipher += message[i]
 
   return encipher
 
 def main(argv):
+  #save input name from arg
   One_time_pad = ''
   unencryptedfile = ''
   encryptedfile = ''
@@ -77,27 +99,35 @@ def main(argv):
     print("too many argument.")
     sys.exit(2)
   for opt,arg in opts:
+    #if command is "-m", print mamual
     if opt == '-m':
       print('mamual: \n -p <size>: generate one-time-pad in given size and store it in "One_time_pad.txt" \n -e <inputfile> -w <padfile> : encrypt the <inputfile> by the <padfile> \n -d <inputfile> -w <padfile> : decipher <inputfile> by the <padfile>')
       sys.exit()
+    #if command is "-p", user input int size, generate one time pad in given size and save it into "One-time-pad.txt"
     elif opt == "-p":
       generatePad(int(arg))
       print("One-time-pad in size", arg, " has been generated and saved in One_time_pad.txt")
       sys.exit()
+    #if command is "-e", user can input filename as unencryptedfile to enciper
     elif opt == "-e":
       unencryptedfile = arg
-    elif opt == "-d":
+    #if command is "-d", user can input filename as encryptedfile to decipher
+    elif opt == "-d": 
       encryptedfile = arg
+    #if command is "-w", with one-time-pad 
     elif opt == "-w":
       One_time_pad = arg
+  #if user input file to encipher and one-time-pad, encipher the given file with One_time_pad
   if unencryptedfile and One_time_pad:
     with open("encrypted-message.txt","w") as file:
       file.write(encipher(unencryptedfile,One_time_pad))
     print("The ", unencryptedfile, " has been encrypted and save to encrypted-message.txt")
+  #if user input file to decipher and one-time-pad, decipher the given file with One_time_pad
   elif encryptedfile and One_time_pad:
     with open("decrypted-message.txt","w") as file:
       file.write(decipher(encryptedfile,One_time_pad))
     print("The ", encryptedfile, " has been decrypted and save to decrypted-message.txt")
+  #incorrect
   else:
     print("Incorrect amount of argument. Please check the manual.")
 
--- unit_test.py
+++ unit_test.py
@@ -1,34 +1,37 @@
 
 from cipher import *
 
-a = generatePad(1000)
-
+#generate One_time_pad in size 1000to test
+generated_pad = generatePad(1000)
+#create txt called encipherTest
+with open("encipherTest.txt", "w") as file:
+  file.write("This is a test for encipher and decipher. Gook Luck! If you see this, Congratulations!")
+#create encrypted-message.txt to test encipher encipherTest.txt by using One_time_pad.txt
 with open("encrypted-message.txt","w") as file:
   file.write(encipher("encipherTest.txt","One_time_pad.txt"))
-
+#use encrypted message in the encrypted-message.txt as decipherTest.txt
 with open("decipherTest.txt", "w") as file:
   file.write((open("encrypted-message.txt")).read())
-
+#create decrypted-message.txt to test decipher decipherTest.txt by using One_time_pad.txt
 with open("decrypted-message.txt", "w") as file:
   file.write(decipher("decipherTest.txt","One_time_pad.txt"))
 
 
-
-    
-  
-
-
+#test genereatPad
 def test_generatePad():
+  #save pad in One_time_pad.txt to pad, if pad equals to generated_pad at the begining, passed test
   with open ("One_time_pad.txt") as file:
     pad = file.read()
-  assert a == pad
-
+  assert generated_pad == pad
+#test decipher
 def test_decipher():
-  d = open("decipherTest.txt").read()
-  d2 = open("encrypted-message.txt").read()
+  #if the information in the decipherTest and decrypted-message is same , passed test
+  d = open("encipherTest.txt").read()
+  d2 = open("decrypted-message.txt").read()
   assert d == d2
-
+#test encipher
 def test_encipher():
+  #if the information in the encipherTest and encrypted-message is same , passed test
   e = open("encrypted-message.txt").read()
   e2 = open("decipherTest.txt").read()
   assert e == e2
